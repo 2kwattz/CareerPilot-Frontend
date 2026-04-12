9088dc6d74bb2d0f8bd56b536161a7083edafabc
Roshan Bhatia <prakashbhatia1970@gmail.com>
2026-04-12 19:32:56 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UtilityBar/utilitybar.jsx
@@ -0,0 +1,27 @@
+import React,{useEffect,useState} from "react";
+
+
+
+const UtilityBar = async () =>{
+
+
+    return(
+        <React.Fragment>
+
+
+            <nav class="utility-bar">
+
+        <div class="utility-bar-content">
+
+            <span>Check out our exciting new launches</span>
+    
+            <span>Get 50% Sale on Bracelets & Stones</span>
+    
+            <span>Get your favorite bracelets now</span>
+
+        </div>
+        
+    </nav>
+        </React.Fragment>
+    )
+}
